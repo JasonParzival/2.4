f29fe994f7e937e0bb184242a1a17f19cbe020c6
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-05 14:14:41 +0800

    Все готово, для выполнения задачи

---
+++ main.js
@@ -0,0 +1,4 @@
+function onButtonClick(btn) {
+    let textField = document.getElementById("textField"); // взял поле по id
+    textField.value = "Привет! =)"; // поменял текст в поле, у полей текст хранится в value
+}
