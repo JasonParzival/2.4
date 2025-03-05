b6818e379d43d6fcfc23a6c44396e2ef2a378f10
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-05 15:08:58 +0800

    Функции работают, все отлично

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function onButtonClick(btn) {
     let newValue = textField.value + btn.innerText;
     textField.value = newValue;
 }
+
+function onButtonClickResult(btn) {
+    let textField = document.getElementById("textField"); 
+    let newValue = textField.value;
+    textField.value = eval(newValue);
+}
